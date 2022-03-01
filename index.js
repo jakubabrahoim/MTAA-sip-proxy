a373374fa3de7dadba62b90dfcfc2f1d8acc7c79
Jakub <jakub.abrahoim@windowslive.com>
2022-03-01 12:31:36 +0100

    oprava bye, uz prichadza

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ var users = {};
 
 proxy.start({port: port, address: address}, (request) => {
     console.log('Incoming from: ' + request.headers.from['uri']);
-    console.log('Method: ' + request.method + '\n');
-
-    console.log(request.status);
+    console.log('Method: ' + request.method);
 
     if (request.method === 'REGISTER') {
         // vytiahnem meno usera
@@ -28,9 +26,10 @@ proxy.start({port: port, address: address}, (request) => {
         users[user] = request.headers.contact;
 
         // vytvorim response
-        var response = sip.makeResponse(request, 200, 'OK');
+        var response = sip.makeResponse(request, 200, 'Vsetko OK');
         response.headers.to.tag = Math.floor(Math.random() * 1e6);
         
+        //console.log(response);
         proxy.send(response);
     } else {
         // vytiahnem meno usera
@@ -39,20 +38,30 @@ proxy.start({port: port, address: address}, (request) => {
         // zistim ci user existuje
         if(users[user] && Array.isArray(users[user]) && users[user].length > 0) {
             request.uri = users[user][0].uri;
-            
-            // proxy posiela 100 Trying
-            var response = sip.makeResponse(request, 100, 'Trying');
 
-            proxy.send(response);
-            proxy.send(request);
+            // potrebujem upravit port, v wiresharku bol zly a potom nechodilo BYE
+            if(request.headers.contact) {
+                request.headers.contact[0].uri = 'sip:' + sip.parseUri(request.headers.contact[0].uri).user + '@' + address + ':' + port.toString();
+            }
+
+            proxy.send(request, (response) => {
+                // pri vlastnom custom callbacku treba tento shift
+                response.headers.via.shift();
+
+                if(response.headers.contact) {
+                    response.headers.contact[0].uri = 'sip:' + sip.parseUri(response.headers.contact[0].uri).user + '@' + address + ':' + port.toString();
+                }
+
+                proxy.send(response);
+            });
+            
         } else {
             // user neexistuje
-            var response = sip.makeResponse(request, 404, 'Not Found');
+            var response = sip.makeResponse(request, 404, 'Nenasiel sa');
 
             proxy.send(response);
         }
     }
 
-    console.log(users);
     console.log('----------------------------------------');
 });
